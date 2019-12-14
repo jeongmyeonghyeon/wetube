8e05baf09638b4ecfe9a06016e6ad791bab82487
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2019-12-14 22:48:23 +0900

    Video search 기능까지

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -11,10 +11,22 @@ export const home = async (req, res) => {
     }
 }
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
     const { query: { term: searchingBy } } = req;
+    let videos = [];
 
-    res.render("search", {pageTitle: "Search", searchingBy });
+    try {
+        videos = await Video.find({
+            title: {
+                $regex: searchingBy,
+                $options: "i",
+            }
+        });
+    } catch(error) {
+        console.log(error);   
+    }
+
+    res.render("search", {pageTitle: "Search", searchingBy, videos });
 }
 
 // export const videos = (req, res) => res.render("videos", {pageTitle: "Videos"});
