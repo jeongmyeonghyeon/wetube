be33028caa1a0cd6056a3c33828fb6a12e7fd50a
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2019-12-17 03:59:41 +0900

    db error

--- src/db.js
+++ src/db.js
@@ -14,7 +14,7 @@ mongoose.connect(
 const db = mongoose.connection;
 
 const handelOpen = () => console.log('[O] Connected to DB');
-const handleError = (error) => console.log(`${typeof process.env.MONGO_URL_NEW}[X] Error on DB Connection ${error}`);
+const handleError = (error) => console.log(`[X] Error on DB Connection ${error}`);
 
 db.once('open', handelOpen);
 db.on('error', handleError);
